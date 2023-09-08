a9b0e58f5cd258a2924b5af0abc12db63792a17d
Alexander Drogin <adrogin@outlook.com>
2023-09-08 11:45:16 +0000

    Deleted draft BCY folder

--- BCY/index.js
+++
@@ -1,7 +0,0 @@
-import {renderGraph} from "./cytograph.js";
-
-function DrawGraph(containerElementName, nodes, edges) {
-    renderGraph(document.getElementById(containerElementName), nodes, edges);
-};
-
-export default DrawGraph;
